25136d6c975fbf4d553971ecee20dad3e1bee30b
liuxiangke0210 <644561703@qq.com>
2016-02-19 22:50:22 +0800

    自定义SQL测试

--- src/main/java/com/springmvcjdbc/controller/UserController.java
+++ src/main/java/com/springmvcjdbc/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.springmvcjdbc.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +24,12 @@ public class UserController {
 	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
 	public ModelAndView welcomePage(ModelAndView model) {
 
-		userDAO.listUser();
+		// userDAO.listUser();
+		List<Map<String, Object>> lists = jdbcDao.queryRowMapListForSql("select * from USER where login_name = ?",
+				new Object[] { "lxk" });
+		for (Map map : lists) {
+			System.out.println(map);
+		}
 		model.setViewName("home");
 		return model;
 	}
