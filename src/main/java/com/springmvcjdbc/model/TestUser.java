7a0877e2272527079d458b422a6a982a86cf150e
lxk <644561703@qq.com>
2016-02-19 16:16:38 +0800

    测试

---
+++ src/main/java/com/springmvcjdbc/model/TestUser.java
@@ -0,0 +1,10 @@
+package com.springmvcjdbc.model;
+
+public class TestUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello git!");
+	}
+
+}
